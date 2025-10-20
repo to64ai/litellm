33bdab2ce826f3403b43e8400993e87201ac4a8f
Shiva Naveen Ravi <shiva.ravi@to64.ai>
2025-10-19 21:18:53 -0700

    feat: rebrand to vpa initial commit

--- ui/litellm-dashboard/src/app/layout.tsx
+++ ui/litellm-dashboard/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LiteLLM Dashboard",
-  description: "LiteLLM Proxy Admin UI",
+  title: "To64 AI VPA Dashboard",
+  description: "VPA Proxy Admin UI",
   icons: { icon: "./favicon.ico" },
 };
 
